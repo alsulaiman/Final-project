eb5241c7c53954ac8ba74957660ee6f48cf6d8fe
hamad alsulaiman <Hamad@hamadsMacBook-2.local>
2018-09-28 02:42:31 -0400

    update DB name

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "12345678",
-    database: "nodemysql"
+    database: "carpool_db"
 });
 
 // Connect to the database
