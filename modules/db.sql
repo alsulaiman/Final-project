b931ddc1e72abad07217fd307068f95ca277b395
hamad alsulaiman <Hamad@hamadsMacBook-2.local>
2018-09-28 21:45:10 -0400

    update Schema

--- modules/db.sql
+++ modules/db.sql
@@ -1,6 +1,5 @@
--- not compleated yet
-
-### Schema
+-- Schema
+DROP DATABASE carpool_db;
 
 CREATE DATABASE carpool_db;
 USE carpool_db;
@@ -9,51 +8,40 @@ CREATE TABLE drivers (
     driver_id INT AUTO_INCREMENT NOT NULL,
     driver_name VARCHAR(255), 
     car VARCHAR(255), 
-    rating 
+    rating INT, 
     email_address VARCHAR(255), 
     driver_password VARCHAR(255), 
-    profile_img VARCHAR(255)
+    profile_img VARCHAR(255),
+    PRIMARY KEY (driver_id)
 );
 
 CREATE TABLE trips (
-    trip_id 
-    driver_id 
-    pickup_location 
-    destination 
-    trip_date DATE(), 
-    trip_time TIME(), 
-    price SMALLMONEY
+    trip_id INT AUTO_INCREMENT NOT NULL,
+    driver_id INT NOT NULL,
+    pickup_location VARCHAR(255),
+    destination VARCHAR(255), 
+    trip_date DATE, 
+    trip_time TIME, 
+    price FLOAT, 
+    PRIMARY KEY (trip_id),
+    FOREIGN KEY (driver_id) REFERENCES drivers(driver_id)
 );
 
 CREATE TABLE riders (
-    rider_id 
-    rider_name 
-    rating 
-    email_address 
-    rider_password 
-    profile_img 
+    rider_id INT AUTO_INCREMENT NOT NULL,
+    rider_name VARCHAR(255), 
+    rating INT, 
+    email_address VARCHAR(255), 
+    rider_password VARCHAR(255), 
+    profile_img VARCHAR(255), 
+    PRIMARY KEY (rider_id)
 );
 
 CREATE TABLE history (
-    history_id 
-    trip_id 
-    rider_id 
+    history_id INT AUTO_INCREMENT NOT NULL,
+    trip_id INT NOT NULL, 
+    rider_id INT NOT NULL, 
+    PRIMARY KEY (history_id),
+    FOREIGN KEY (trip_id) REFERENCES trips(trip_id),
+    FOREIGN KEY (rider_id) REFERENCES riders(rider_id)
 );
-
-CREATE TABLE trips
-(
-    id INT AUTO_INCREMENT NOT NULL, 
-	driver_id varchar(255) NOT NULL,
-	rider_id varchar(255) NOT NULL,
-	pickup_location varchar(255) NOT NULL,
-	destination varchar(255) NOT NULL,
-	trip_DATE(),DATETIME,
-	trip_DATE(),varchar(255) NOT NULL,
-	pickup_time varchar(255) NOT NULL,
-	car_model varchar(255) NOT NULL,
-	price varchar(255) NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (driver_id) REFERENCES Persons(driver_id),
-	FOREIGN KEY (rider_id) REFERENCES Persons(rider_id)
-);
-
